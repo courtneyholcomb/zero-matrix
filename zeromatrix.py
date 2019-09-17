e06b2fb050b77589ea35db2dc7020351304cb754
Courtney Holcomb <courtneyeholcomb@gmail.com>
2019-09-16 17:34:17 -0700

    added explanatory comments

--- zeromatrix.py
+++ zeromatrix.py
@@ -22,6 +22,7 @@ def zero_matrix(matrix):
 
     zero_indices = []
 
+    # if row includes 0, save that index, then convert whole row to 0
     for row in matrix:
         if 0 in row:
             for i, item in enumerate(row):
@@ -29,6 +30,7 @@ def zero_matrix(matrix):
                     zero_indices.append(i)
                 row[i] = 0
 
+    # for for all zero_indices, convert each row at that index to 0
     for index in zero_indices:
         for row in matrix:
             row[index] = 0
